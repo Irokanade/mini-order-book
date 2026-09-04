d0d941491a3848abdf81977ba09d0a9c2c632fd5
Irokanade <michaeljs.leong@gmail.com>
2026-09-04 15:39:32 +0800

    feat: implement append and remove methods for limit object

--- limit_order_book.h
+++ limit_order_book.h
@@ -24,6 +24,49 @@ struct Limit {
     int limit_price;
     int size;
     int total_volume;
+
+    Limit(int _limit_price, int _size, int _total_volume) :
+            parent(nullptr), left_child(nullptr), right_child(nullptr),
+            head_order(new Order), tail_order(new Order),
+            limit_price(_limit_price), size(_size), total_volume(_total_volume) {
+        head_order->next_order = tail_order;
+        tail_order->prev_order = head_order;
+    }
+
+    ~Limit() {
+        // delete sentinels
+        delete head_order;
+        delete tail_order;
+    }
+
+    Limit(const Limit &) = delete;
+    Limit &operator=(const Limit &) = delete;
+    Limit(Limit &&) = delete;
+    Limit &operator=(Limit &&) = delete;
+
+    void append(Order *order) {
+        order->next_order = tail_order;
+        order->prev_order = tail_order->prev_order;
+
+        tail_order->prev_order->next_order = order;
+        tail_order->prev_order = order;
+
+        size++;
+        total_volume += order->shares;
+        order->parent_limit = this;
+    }
+
+    void remove(Order *order) {
+        Order *prev_order = order->prev_order;
+        Order *next_order = order->next_order;
+
+        prev_order->next_order = next_order;
+        next_order->prev_order = prev_order;
+
+        size--;
+        total_volume -= order->shares;
+        order->parent_limit = nullptr;
+    }
 };
 
 struct Book {
