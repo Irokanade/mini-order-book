8f6af45df7c56546ef56f145fdd3583aef07e3b0
Irokanade <michaeljs.leong@gmail.com>
2026-09-14 14:48:19 +0800

    chore: remove DS Store from git

---
+++ build-profile/CMakeFiles/mini_order_book.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/mini_order_book.dir/main.cpp.o"
+  "CMakeFiles/mini_order_book.dir/main.cpp.o.d"
+  "mini_order_book"
+  "mini_order_book.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/mini_order_book.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
